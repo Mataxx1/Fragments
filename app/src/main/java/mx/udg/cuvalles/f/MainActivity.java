e28df4e25f639a764f3ff29181b04274a31834fd
Mataxx1 <ejrgm1991@gmail.com>
2017-03-28 17:33:59 -0600

    Para prácitca de los fragments

--- app/src/main/java/mx/udg/cuvalles/f/MainActivity.java
+++ app/src/main/java/mx/udg/cuvalles/f/MainActivity.java
@@ -4,13 +4,19 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import mx.udg.cuvalles.fragments.BlankFragment;
 import mx.udg.cuvalles.fragments.R;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements BlankFragment.DataListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void sendData(String text) {
+
     }
 }
--- app/src/main/java/mx/udg/cuvalles/fragments/BlankFragment.java
+++ app/src/main/java/mx/udg/cuvalles/fragments/BlankFragment.java
@@ -1,11 +1,15 @@
 package mx.udg.cuvalles.fragments;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -13,17 +17,48 @@ import android.view.ViewGroup;
  */
 public class BlankFragment extends Fragment {
 
-
+    private EditText etDatos;
+    private Button btnEnviar;
+    private DataListener callback;
     public BlankFragment() {
         // Required empty public constructor
     }
 
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try{
+            callback = (DataListener) context;
+        }catch (Exception e){
+            throw new ClassCastException(context.toString()+" No se puede implementar DataListener");
+        }
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        View vista=inflater.inflate(R.layout.fragment_blank, container, false);
+
+        etDatos = (EditText) vista.findViewById(R.id.textoInput);
+        btnEnviar = (Button) vista.findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            String textToSend = etDatos.getText().toString();
+                callback.sendData(textToSend);
+            }
+        });
+
+
+        return vista;
     }
 
+    public interface DataListener{
+        void sendData(String text);
+
+    }
 }
